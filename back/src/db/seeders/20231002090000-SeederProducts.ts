14e512c5ebf4ea05e24a022fd3ee232c847aecc6
Andrés López <andreslop@gmail.com>
2023-10-02 10:05:27 +0200

    add product seeder

---
+++ back/src/db/seeders/20231002090000-SeederProducts.ts
@@ -0,0 +1,38 @@
+import { QueryInterface } from 'sequelize';
+
+interface productData {
+  title: string;
+  description: string;
+  category: string;
+  image: string;
+}
+
+module.exports = {
+  async up(queryInterface: QueryInterface) {
+    try {
+      const response = await fetch('https://fakestoreapi.com/products');
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const products: productData[] = await response.json();
+
+      const productData = products.map((product) => ({
+        title: product.title,
+        description: product.description,
+        category: product.category,
+        imageUrl: product.image,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }));
+
+      await queryInterface.bulkInsert('product', productData, {});
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error('Error seeding product data:', error);
+    }
+  },
+
+  async down(queryInterface: QueryInterface) {
+    return queryInterface.bulkDelete('product', null, {});
+  },
+};
