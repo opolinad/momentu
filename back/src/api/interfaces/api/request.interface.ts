4e176143023f9ca74583e2b9d29062f696895db4
Andrés López <andreslop@gmail.com>
2023-10-02 11:25:19 +0200

    asdd product reques interface

--- back/src/api/interfaces/api/request.interface.ts
+++ back/src/api/interfaces/api/request.interface.ts
@@ -1,6 +1,11 @@
 import { Request } from 'express';
 import User from '../../../db/models/user.model';
+import Product from '../../../db/models/product.model';
 
 export interface userRequest extends Request {
   user: User;
+}
+
+export interface productRequest extends Request {
+  product: Product;
 }
