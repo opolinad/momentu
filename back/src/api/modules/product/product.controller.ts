35071d8b7190558b75bf2146e5b9d1ef8a69ec17
Andrés López <andreslop@gmail.com>
2023-10-02 10:04:55 +0200

    add pagination to products

--- back/src/api/modules/product/product.controller.ts
+++ back/src/api/modules/product/product.controller.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from 'express';
 import { apiResponse } from '../../utils/responses/apiResponse.response';
 import { getProducts } from './product.service';
+import { paginationItems } from '../../utils/pagination/pagination.utils';
 
 export const getAllProducts = async (
   req: Request,
   res: Response,
 ): Promise<void> => {
-  const responseObject = await getProducts();
+  const { page, perPage } = req.query;
+  const pagination = paginationItems(page as string, perPage as string);
+  const responseObject = await getProducts(pagination);
   apiResponse(res, responseObject);
 };
--- back/src/api/modules/product/product.service.ts
+++ back/src/api/modules/product/product.service.ts
@@ -2,19 +2,38 @@ import Product from '../../../db/models/product.model';
 import { httpStatusCode } from '../../interfaces/api/httpStatusCodes.interface';
 import { response } from '../../interfaces/api/response.interface';
 import {
+  pagination,
+  paginationResults,
+  results,
+} from '../../interfaces/pagination/pagination.interface';
+import { BusinessException } from '../../utils/errors/bussinessException.errors';
+import { paginateResults } from '../../utils/pagination/pagination.utils';
+import {
   buildResponseInternalErrorObject,
   buildResponseObject,
 } from '../../utils/responses/apiResponse.response';
 
-export const getProducts = async (): Promise<
-  response<null | { products: Product[] }>
+export const getProducts = async (
+  paginationItems: pagination,
+): Promise<
+  response<null | unknown | { products: paginationResults<results> }>
 > => {
   try {
-    const products = await Product.findAll({ raw: true });
-    return buildResponseObject(httpStatusCode.OK, 'Products retrieved', {
+    const { limit, offset, pageNumber } = paginationItems;
+    const products = await Product.findAndCountAll({ limit, offset });
+    const productsPaginated = paginateResults(
       products,
+      limit,
+      pageNumber,
+      'product',
+    );
+
+    return buildResponseObject(httpStatusCode.OK, 'Products retrieved', {
+      products: productsPaginated,
     });
   } catch (error) {
+    if (error instanceof BusinessException) return error.getResponseObject();
+
     return buildResponseInternalErrorObject();
   }
 };
